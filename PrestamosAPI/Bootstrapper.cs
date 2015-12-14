238e294af4264ed0e4a3ff68396bfd62e83fa21f
icnovaro <icnovaro@gmail.com>
2015-12-14 15:50:56 -0500

    Agrega archivos iniciales del proyecto

---
+++ PrestamosAPI/Bootstrapper.cs
@@ -0,0 +1,11 @@
+﻿using Nancy;
+
+namespace PrestamosAPI
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        // The bootstrapper enables you to reconfigure the composition of the framework,
+        // by overriding the various methods and properties.
+        // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+    }
+}
---
+++ PrestamosAPI/Modules/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace PrestamosAPI.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
